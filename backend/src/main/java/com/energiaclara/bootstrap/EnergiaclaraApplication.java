5e345547cdf5aeffdf03f979cf8ec005966d5c83
Paulo-05-mc <a2022212002@estudiantes.upsa.edu.bo>
2026-05-25 18:03:33 -0400

    ai servicee

--- backend/src/main/java/com/energiaclara/bootstrap/EnergiaclaraApplication.java
+++ backend/src/main/java/com/energiaclara/bootstrap/EnergiaclaraApplication.java
@@ -1,4 +1,4 @@
- package com.energiaclara.bootstrap;
+package com.energiaclara.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
